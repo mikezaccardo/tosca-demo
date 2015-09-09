f9e1a5b79341037ffc87ac9fc2c1dcf76d0555ab
Alex Heneveld <alex.heneveld@cloudsoftcorp.com>
2015-09-09 19:44:17 +0100

    simple demo of a script starting a web server

---
+++ script1/scripts/configure.sh
@@ -0,0 +1,12 @@
+      # create the web page to serve
+      cat > index.html << EOF
+      
+      Hello world.
+      <p>
+      I am a TOSCA example.
+      <p>
+      Created at: `date`
+      <p>
+      
+      EOF
+
